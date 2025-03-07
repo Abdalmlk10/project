192dd9a2c805ccbe022ee44ba7cc1fe686425d75
Mojeeb Almahmoudi <141680741+MojeebAlmahmoudi@users.noreply.github.com>
2025-03-07 04:44:53 +0300

    Login (#8)
    * login screin
    
    * register screin
    
    * my-account.php  screin
    
    ---------
    
    Co-authored-by: MojeebAlmahmoudi <mojeebalmahmuodi2@gmail.com>

---
+++ my-account.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-US">
+
+<head>
+  <title>SUPER_PROJECT</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="author" content="surfside media" />
+  <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon">
+  <link rel="preconnect" href="https://fonts.gstatic.com/">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&amp;display=swap"
+    rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Allura&amp;display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/plugins/swiper.min.css" type="text/css" />
+  <link rel="stylesheet" href="assets/css/style.css" type="text/css" />
+  <link rel="stylesheet" href="assets/css/custom.css" type="text/css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous" referrerpolicy="no-referrer">
+</head>
+
+
+<body class="gradient-bg">
+
+  <main class="pt-90">
+    <div class="mb-4 pb-4"></div>
+    <section class="my-account container">
+      <h2 class="page-title">My Account</h2>
+      <div class="row">
+        <div class="col-lg-3">
+          <ul class="account-nav">
+            <li><a href="my-account.php" class="menu-link menu-link_us-s">Dashboard</a></li>
+            <li><a href="account-orders.php" class="menu-link menu-link_us-s">Orders</a></li>
+            <li><a href="account-address.php" class="menu-link menu-link_us-s">Addresses</a></li>
+            <li><a href="account-details.php" class="menu-link menu-link_us-s">Account Details</a></li>
+            <li><a href="account-wishlist.php" class="menu-link menu-link_us-s">Wishlist</a></li>
+            <li><a href="login.php" class="menu-link menu-link_us-s">Logout</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-9">
+          <div class="page-content my-account__dashboard">
+            <p>Hello <strong>User</strong></p>
+            <p>From your account dashboard you can view your <a class="unerline-link" href="account_orders.php">recent
+                orders</a>, manage your <a class="unerline-link" href="account_edit_address.php">shipping
+                addresses</a>, and <a class="unerline-link" href="account_edit.php">edit your password and account
+                details.</a></p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+
+</body>
+
+</html>
