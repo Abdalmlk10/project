4378d0f7ee66bf9ff2b7442ca10ca8e292c8dca6
MOATAZALAQIL <moataznajialaqil@gmail.com>
2025-03-06 17:53:12 -0800

    Update about.php
    add header and footer to about page

--- about.php
+++ about.php
@@ -20,7 +20,7 @@
 
 
 <body class="gradient-bg">
-
+  <?php include 'header.php' ?>
   <main class="pt-90">
     <div class="mb-4 pb-4"></div>
     <section class="contact-us container">
@@ -72,7 +72,7 @@
 
   </main>
 
-
+<?php include 'footer.php' ?>
 
 
 
