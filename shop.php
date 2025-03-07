605eb33485e9989df3155c9837a62f55b11250fb
Dianamohammed <pdpdrtrt@gmail.com>
2025-03-06 18:42:57 -0800

    Update shop page

--- shop.php
+++ shop.php
@@ -20,7 +20,7 @@
 
 
 <body class="gradient-bg">
- 
+ <?php include 'header.php' ?>
   <main class="pt-90">
     <section class="shop-main container d-flex pt-4 pt-xl-5">
       <div class="shop-sidebar side-sticky bg-body" id="shopFilter">
@@ -871,7 +871,7 @@
       </div>
     </section>
   </main>
-
+<?php include 'footer.php' ?>
   
 </body>
 
